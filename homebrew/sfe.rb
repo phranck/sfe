69cf8df9c93ee6612912cca6042f5543e51e2bc6
phranck <phranck@phranck.dev>
2026-02-11 15:21:17 +0100

    Update Homebrew formula to v1.0.1

--- homebrew/sfe.rb
+++ homebrew/sfe.rb
@@ -1,8 +1,8 @@
 class Sfe < Formula
   desc "SF Symbols Extractor - Extract individual SVG files from SF Symbols 7.2"
   homepage "https://github.com/phranck/sfe"
-  url "https://github.com/phranck/sfe/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "3774b05ad4c1ea0e132a34a55c17b05b0956417524d871fd038fad725880fd2a"
+  url "https://github.com/phranck/sfe/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "fac0a278d36d69281b42aba747fcdd9617ee329e4b187e9afe264ced44745766"
   license "MIT"
 
   def install
