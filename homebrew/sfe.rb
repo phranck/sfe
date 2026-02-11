0461c0dec144ad4821c2b7b1098ce8dd81a396fc
phranck <phranck@phranck.dev>
2026-02-11 17:30:58 +0100

    Chore: Update SHA256 for v1.1.0 with --version flag

--- homebrew/sfe.rb
+++ homebrew/sfe.rb
@@ -2,7 +2,7 @@ class Sfe < Formula
   desc "SF Symbols Extractor - Extract individual SVG files with embedded metadata from SF Symbols 7.2"
   homepage "https://github.com/phranck/sfe"
   url "https://github.com/phranck/sfe/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "7d0858fb2ddeced14e338afa88fff3ff198782462f7e773deed4c8bcfc5ff971"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   def install
