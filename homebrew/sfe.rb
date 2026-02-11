5818d909d3e473cd806208bf535237f501c6d2b0
phranck <phranck@phranck.dev>
2026-02-11 17:31:58 +0100

    Fix: Correct SHA256 for v1.1.0

--- homebrew/sfe.rb
+++ homebrew/sfe.rb
@@ -2,7 +2,7 @@ class Sfe < Formula
   desc "SF Symbols Extractor - Extract individual SVG files with embedded metadata from SF Symbols 7.2"
   homepage "https://github.com/phranck/sfe"
   url "https://github.com/phranck/sfe/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "c6dad117b1e68733ce38e18fffc6c05e0d941f01ee706d581005b19aff99cf2c"
   license "MIT"
 
   def install
